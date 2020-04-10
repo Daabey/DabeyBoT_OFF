ebf61db164e734ae932a88e12079d0bfd3782abb
Daabey <38768774+Daabey@users.noreply.github.com>
2020-04-10 02:37:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const low = require('lowdb')
-const FlieSync = require('lowdb/adapters/FileSync')
 
 const adapetr = new FlieSync('database.json');
-const db = low(adapeter);
 
 db.defaults({ histoires: [], xp: []}).write()
 
